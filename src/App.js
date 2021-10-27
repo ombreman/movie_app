ad025fa752ca60651aa23b261cf64b56665976a2
ombreman <ombreman21@gmail.com>
2021-10-27 23:24:24 +0100

    Add Food component to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Potato from './Potato';
+
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello fuckers!!!</h1>
-      <Potato />
+      <h1>앙념치킨</h1>
+      <Food fav="김치" />
+      <Food fav="라면" />
+      <Food fav="삼겹살" />
+      <Food fav="목살" />
     </div>
   );
 }
