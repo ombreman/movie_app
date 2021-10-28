cf94ec1bd0779e69c7616b9f33e2f9ec99832cef
ombreman <ombreman21@gmail.com>
2021-10-28 11:31:11 +0100

    Map Food component in App component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>
+function Food({ name, picture }) {
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture} />
+  </div>
 }
+
+const foodILike = [
+  {
+    name: "김치",
+    image: "https://w.namu.la/s/a85e178b2233fa1e3461ae51dba6757e8cc87b215ea326660c85904f61daf0dcd84f8b8733cee71ca34fa743822d5aa1650766f6b2a90118edc5e76974e8305263694a4c2358508602ad7694e2cd022389545b2812f6e86bf29e05e172b53405"
+  },
+  {
+    name: "비빔밥",
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2S8riFnNgOm55O3kZjZiDgHaFG%26pid%3DApi&f=1"
+  }
+]
 
 function App() {
   return (
     <div>
-      <h1>앙념치킨</h1>
-      <Food fav="김치" />
-      <Food fav="라면" />
-      <Food fav="삼겹살" />
-      <Food fav="목살" />
+      {foodILike.map(dish => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
