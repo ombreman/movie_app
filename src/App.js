237dc72ecdf5b22bb06a6b249ef4ec99f5963c54
ombreman <ombreman21@gmail.com>
2021-10-28 16:22:32 +0100

    Finish practice of React Props

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React from 'react';
-
-function Food({ name, picture }) {
-  return <div>
-    <h2>I like {name}</h2>
-    <img src={picture} />
-  </div>
-}
+import PropTypes from 'prop-types';
 
 const foodILike = [
   {
+    id: 1,
     name: "김치",
-    image: "https://w.namu.la/s/a85e178b2233fa1e3461ae51dba6757e8cc87b215ea326660c85904f61daf0dcd84f8b8733cee71ca34fa743822d5aa1650766f6b2a90118edc5e76974e8305263694a4c2358508602ad7694e2cd022389545b2812f6e86bf29e05e172b53405"
+    image: "https://w.namu.la/s/a85e178b2233fa1e3461ae51dba6757e8cc87b215ea326660c85904f61daf0dcd84f8b8733cee71ca34fa743822d5aa1650766f6b2a90118edc5e76974e8305263694a4c2358508602ad7694e2cd022389545b2812f6e86bf29e05e172b53405",
+    rating: 3.5
   },
   {
+    id: 2,
     name: "비빔밥",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2S8riFnNgOm55O3kZjZiDgHaFG%26pid%3DApi&f=1"
+    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2S8riFnNgOm55O3kZjZiDgHaFG%26pid%3DApi&f=1",
+    rating: 4.5
   }
 ]
+
+function Food({ name, picture, rating }) {
+  return <div>
+    <h2>I like {name}</h2>
+    <h4>{rating}/5.0</h4>
+    <img src={picture} alt={name} />
+  </div>
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+}
 
 function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
       ))}
     </div>
   );
